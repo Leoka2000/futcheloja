52972eea6d1af5414323848013a172b85ae195aa
Leoka2000 <reusoliveiraleo@gmail.com>
2025-04-21 14:39:36 +0200

    proxy

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -52,7 +52,7 @@ class AdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
-                UpgradeInsecureRequestsMiddleware::class,
+
 
             ])
             ->authMiddleware([
--- app/Providers/ForceHttpsProvider.php
+++ app/Providers/ForceHttpsProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -11,6 +12,16 @@ class ForceHttpsProvider extends ServiceProvider
     {
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
+
+            // Trust all proxy headers
+            $this->app->make(Request::class)->setTrustedProxies(
+                ['0.0.0.0/0'],
+                Request::HEADER_X_FORWARDED_FOR |
+                    Request::HEADER_X_FORWARDED_HOST |
+                    Request::HEADER_X_FORWARDED_PORT |
+                    Request::HEADER_X_FORWARDED_PROTO |
+                    Request::HEADER_X_FORWARDED_AWS_ELB
+            );
         }
     }
 }
