b257294aef77e1e34999722294a2b23435bfef87
Leoka2000 <reusoliveiraleo@gmail.com>
2025-04-27 22:26:30 +0200

    pagination

--- resources/views/livewire/shopping-cart-component.blade.php
+++ resources/views/livewire/shopping-cart-component.blade.php
@@ -23,7 +23,7 @@ new class extends Component {
             $query->where('name', 'like', '%' . $this->searchTerm . '%');
         }
 
-        return $query->paginate(8);
+        return $query->paginate(6);
     }
 
     public function setFilter($category)
